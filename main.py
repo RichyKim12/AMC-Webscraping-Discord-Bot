362f917730c7d364e8337eb7689ce1b4a9c08d86
RichyKim12 <109635077+RichyKim12@users.noreply.github.com>
2022-08-06 01:55:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
-import discord
-import random
+import discord #Lets us code in discords bot API
+#from webscrape import *
 
-TOKEN = 'MTAwNDAzMTUyNTc2NTAwOTQxOA.GYyixs.GoLGv1x7hTrEjA4-rkfLVoVYe_7tWKH_ZHEVWA'
+
+import bs4
+from bs4 import BeautifulSoup #web scraping websites using HTML and XML
+import requests #Making Http requests and parsing data
+
+#testing bs4 code
+
+
+
+
+
+TOKEN = 'MTAwNDAzMTUyNTc2NTAwOTQxOA.GYyixs.GoLGv1x7hTrEjA4-rkfLVoVYe_7tWKH_ZHEVWA' #personal token don't share online
 
 client = discord.Client()
 
+
+# method to make sure bot is successfully running
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     
+# chat commands 
 @client.event
 async def on_message(message):
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     
     channel = str(message.channel.name)
-    print(f'{username}: {user_message} ({channel})')
-    if message.author == client.user:
+    print(f'{username}: {user_message} ({channel})')#chat log printing
+    
+    if message.author == client.user:# prevents infinite looping of bot talking to itself
         return 
-    if message.channel.name == 'homework-help':
-        if user_message.lower() == 'shut up':
-            await message.channel.send(f'Eat my ass {username}! :(')
     if message.channel.name == 'bot-testing':
-        if user_message.lower() == 'single':
-        #if 'single' in user_message.lower():
-            await message.channel.send(f'lmao mohit is single ong fr')
-            return 
-        elif user_message.lower() == 'bye':
+        if user_message.lower() == '!bye':
             await message.channel.send(f'Bye {username}!')
             return 
-        elif user_message.lower() == '!random':
-            response = f'This is your random number: {random.randrange(10000)}'
-            await message.channel.send(response)
-            return 
-        elif user_message.lower() == '!advice':
-            await message.channel.send(f'Learn some Python {username}.')
-            return
+    
+        
+
 client.run(TOKEN)
