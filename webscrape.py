ab7b3422296479fe119e385f24a2631c9a7c5785
RichyKim12 <rmk9ds@virginia.edu>
2022-11-02 17:58:53 -0400

    Reintroduced SQLite3 database
    Forgot to commit after implementing SQLite3 database into code, reimplemented it.

--- webscrape.py
+++ webscrape.py
@@ -2,78 +2,71 @@ from sys import breakpointhook
 from bs4 import BeautifulSoup # web scraping websites using HTML and XML
 import requests  # Making Http requests and parsing data
 import re
+import sqlite3
+
+conn = sqlite3.connect('movie.db')
+c = conn.cursor()
+
+# Creates table
+c.execute("""CREATE TABLE IF NOT EXISTS movies
+            (search text PRIMARY KEY,
+            title text,
+            img_link text,
+            movie_link text,
+            runtime text,
+            release_date text,
+            movie_description text)""")
+
+
 
 html_text = requests.get('https://www.amctheatres.com/movies').text #if you dont use ".text" then you be passing data as bits instead of text
 soup = BeautifulSoup(html_text, 'lxml')
 movies = soup.findAll('div', class_ = "Slide")
 
 
-# Dictionary:
-#    - key: movietitle
-#    - val[0]: movie title
-#    - val[1]: img link
-#    - val[2]: movie link
-#    - val[3]: runtime
-#    - val[4]: Release date
-#    - val[5]: Movie description
-moviedict = {}
 
+# for movie in movies:
+#     moviePosterContent = movie.find('div', class_ = 'PosterContent') #var used to further dive into HTML to look for movie title
+    
+    
+#     # MOVIE TITLE
+#     movietitle = moviePosterContent.find('h3').text #need to use .text method otherwise it will return HTML syntax 
+#     search_temp = (movietitle.lower()).replace(" ","") # gets rid of white spaces and makes all letters lowercase
+#     search_title = re.sub(r'[^a-zA-Z0-9]', '', search_temp) # gets rid of all alphanumeric characters
+#     print(movietitle)
+    
+#     # MOVIE IMAGE
+#     movieimg = movie.find('img') #finds all data starting with img
+#     movieimgsrc = movieimg['src']
+    
+    
+    
+#     # MOVIE LINK
+#     movielinktemp1 = movie.find('a')
+#     moviepartiallink = movielinktemp1['href'] #this for some reason doesn't include the https://www.amctheatres.com part of the link
+#     movielink = 'https://www.amctheatres.com' + moviepartiallink # This movie link will then also be webscraped to scrape movie description data
+     
+#     # MOVIE DESCRIPTION
+#     html_text2 = requests.get(movielink).text
+#     soup = BeautifulSoup(html_text2, 'lxml')
+#     nodey = soup.find('div', class_ = 'Intro-text col-md-8 order-md-last')
+#     nodey2 = nodey.find('p', itemprop = 'description', class_ = 'show-text')
+#     moviedesc = nodey2.text
 
-#How to store all this data, for now store it in dictionary
-for movie in movies:
-    moviePosterContent = movie.find('div', class_ = 'PosterContent') #var used to further dive into HTML to look for movie title
-    moviedata = []
+#     # MOVIE TIME
+#     movietimetemp1 = moviePosterContent.find('div', class_ = 'Headline--eyebrow')
+#     # node = movietimetemp1.find('p')
+#     node = movietimetemp1
     
-    # MOVIE TITLE
-    movietitle = moviePosterContent.find('h3').text #need to use .text method otherwise it will return HTML syntax 
-    #print(movietitle)
-    temp = (movietitle.lower()).replace(" ","") # gets rid of white spaces and makes all letters lowercase
-    temp = re.sub(r'[^a-zA-Z0-9]', '', temp) # gets rid of all alphanumeric characters
-    moviedict[temp] = []
-    
-    moviedata.append(movietitle)
-    # MOVIE IMAGE
-    movieimg = movie.find('img') #finds all data starting with img
-    movieimgsrc = movieimg['src']
-    #print(movieimgsrc) #indexing using 'src' to find image source link
-    moviedata.append(movieimgsrc)
-    
-    
-    # MOVIE LINK
-    movielinktemp1 = movie.find('a')
-    moviepartiallink = movielinktemp1['href'] #this for some reason doesn't include the https://www.amctheatres.com part of the link
-    movielink = 'https://www.amctheatres.com' + moviepartiallink # This movie link will then also be webscraped to scrape movie description data
-   # print(movielink)
-    moviedata.append(movielink)
-    
-    # MOVIE TIME
-    movietimetemp1 = moviePosterContent.find('div', class_ = 'Headline--eyebrow')
-    # node = movietimetemp1.find('p')
-    node = movietimetemp1
-    
-    if node.text == '': #if statement to skip over empty description fields
-        continue
-    else:
-        movietime = node.find('span', class_ = 'js-runtimeConvert u-inlineFlexCenter').text
-        movierelease = node.find('span', class_ = 'MoviePosters__released-month').text
-        # print(movietime)
-        # print(movierelease)
-        moviedata.append(movietime)
-        moviedata.append(movierelease)
-    moviedict[temp].append(moviedata)
-
-
-#navigate to the movie description link and web scrape the description box
-for k,v in moviedict.items():
-    if len(v) > 0:
-        moviedesclink = v[0][2]
-        html_text2 = requests.get(moviedesclink).text
-        soup = BeautifulSoup(html_text2, 'lxml')
-        nodey = soup.find('div', class_ = 'Intro-text col-md-8 order-md-last')
-        nodey2 = nodey.find('p', itemprop = 'description', class_ = 'show-text')
-        moviedict[k][0].append(nodey2.text)
-
-
-for k,v in moviedict.items():
-    print(k,v)
-    
+#     if node.text == '': #if statement to skip over empty description fields
+#         continue
+#     else:
+#         movietime = node.find('span', class_ = 'js-runtimeConvert u-inlineFlexCenter').text
+#         movierelease = node.find('span', class_ = 'MoviePosters__released-month').text
+#         c.execute('''INSERT OR IGNORE INTO movies VALUES(?,?,?,?,?,?,?)''',\
+#                  (search_title,movietitle,movieimgsrc,movielink,movietime,movierelease, moviedesc))
+conn.commit()     
+# c.execute("SELECT * FROM movies WHERE search='amsterdam'") 
+# print(c.fetchone())  
+# c.close()
+       
